d422f3d7a6f86a7b12d4249e04a94dc8180d9274
Павел Петров <55802681+Furton@users.noreply.github.com>
2023-07-17 22:27:33 +0300

    fix

--- Assets/Yandex/Scripts/Yandex/Sound/SoundButton.cs
+++ Assets/Yandex/Scripts/Yandex/Sound/SoundButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SoundButton : MonoBehaviour
+public class SoundButton: MonoBehaviour
 {
     public Sprite soundIsOn, soundIsOff;
     public Data data;
